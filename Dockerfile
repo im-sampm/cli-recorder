755e2ed8473f95d63e9e71a98b79f87fc5df11b6
Sam Peddamatham <sam.pm@infomagnus.com>
2024-02-13 17:27:05 -0800

    Initial commit.

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM python:3.9.18-slim-bullseye
+WORKDIR /home
+
+RUN apt-get update && apt-get install -y git pv
+RUN pip3 install termtosvg
+RUN git clone https://github.com/paxtonhare/demo-magic.git
---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+########################
+# include the magic
+########################
+. ./demo-magic/demo-magic.sh
+
+DEMO_PROMPT="${GREEN}➜ ${CYAN}\W ${COLOR_RESET}"
+TYPE_SPEED=10
+
+# hide the evidence
+clear
+
+# Put your stuff here
+pe "ls -l"
